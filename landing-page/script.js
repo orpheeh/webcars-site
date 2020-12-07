0ace06b19cb4f0dcfde4105165828d6f631c6b28
Nve Hounkponou Orphee <orpheenve@hotmail.com>
2020-12-07 10:28:08 +0100

    feat save

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,3 +1,7 @@
 document.querySelector('.humbergers').addEventListener('click', () => {
     document.querySelector('nav').classList.toggle('show-nav-item');
 })
+
+function hideNav(){
+    document.querySelector('nav').classList.remove('show-nav-item');
+}
